f180221039003f0a3f1442836cc767b9bad00b93
Guga1010 <joaogustavosdleo@gmail.com>
2025-11-26 17:16:34 -0300

    atualizando 26_11

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $credenciais = $request->all('email', 'password');
+        $token = auth('api')->attempt($credenciais);
+
+        if ($token) {
+            return response()->json(['token' => $token]);
+        } else {
+            return response()->json(['erro' => 'Usuário ou senha inválida!'], 403);
+        }
+
+        return 'login';
+    }
+
+    public function logout(){
+        return 'logout';
+    }
+
+    public function refresh(){
+        return 'refresh';
+    }
+
+    public function me(){
+        return 'me';
+    }
+}
